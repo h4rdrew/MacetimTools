7d384869d53f061306b2e3d1bfb53bbf82920303
Mateus Moraes <44010175+h4rdrew@users.noreply.github.com>
2020-12-01 04:19:17 -0300

    Backup

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Windows.Forms;
+using MacetimTools.Class;
 
 namespace MacetimTools
 {
@@ -11,9 +15,16 @@ namespace MacetimTools
         [STAThread]
         static void Main()
         {
+            EmbeddedAssembly.Load("MacetimTools.System.Data.SQLite.dll", "System.Data.SQLite.dll");
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+        static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return EmbeddedAssembly.Get(args.Name);
         }
     }
 }
